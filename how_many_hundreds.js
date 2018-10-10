42bfe1530026ff814736c8a33025940da970ea3e
Eric Wilson <ericswilson94@gmail.com>
2018-10-10 22:11:17 +0000

    how_many_hundreds.js exercise with 'howManyHundreds' function completed

---
+++ how_many_hundreds.js
@@ -0,0 +1,11 @@
+function howManyHundreds(number){
+  var output = number / 100;
+  var rounddown = Math.floor(output);
+  return rounddown;
+}
+
+console.log(howManyHundreds(1000), "=?", 10);
+console.log(howManyHundreds(894), "=?", 8);
+console.log(howManyHundreds(520), "=?", 5);
+console.log(howManyHundreds(99), "=?", 0);
+console.log(howManyHundreds(0), "=?", 0);
